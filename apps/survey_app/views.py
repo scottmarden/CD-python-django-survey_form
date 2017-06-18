303ce1a86099fdb82077210906a339d5e9c6f8c2
Scott <marden.scott@gmail.com>
2017-06-18 12:05:40 -0700

    Finished assignment. No styling at this time, focused on function of taking information via request.POST and transfering it to request.session, then filling html template based on that session data.

--- apps/survey_app/views.py
+++ apps/survey_app/views.py
@@ -1,6 +1,28 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
-from django.shortcuts import render
+from django.shortcuts import render, redirect, HttpResponse
+from django import forms
 
 # Create your views here.
+def index(request):
+	return render(request, 'survey_app/index.html')
+
+def submit(request):
+	print (request.method)
+	if request.method == 'POST':
+		print "*"*50
+		request.session['name'] = request.POST['name']
+		request.session['location'] = request.POST['location']
+		request.session['language'] = request.POST['language']
+		request.session['comments'] = request.POST['comments']
+		print request.session['name']
+		print request.session['location']
+		print request.session['language']
+		print request.session['comments']
+		print "*"*50
+	return redirect('/results')
+
+def results(request):
+	
+	return render(request, 'survey_app/result.html')
